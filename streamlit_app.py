dcc4e41eae0c30378664e4b406576c2f6875e36b
Sirajo Muazu Kallo <119134229+sirajoka@users.noreply.github.com>
2023-09-18 18:46:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,38 @@
-st.title("HealthCare ChatBot 🧑🏽‍⚕️")
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+
+#load the pdf files from the path
+loader = DirectoryLoader('data/',glob="*.pdf",loader_cls=PyPDFLoader)
+documents = loader.load()
+
+#split text into chunks
+text_splitter  = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+text_chunks = text_splitter.split_documents(documents)
+
+#create embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                   model_kwargs={'device':"cpu"})
+
+#vectorstore
+vector_store = FAISS.from_documents(text_chunks,embeddings)
+
+#create llm
+llm = CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",model_type="llama",
+                    config={'max_new_tokens':128,'temperature':0.01})
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+chain = ConversationalRetrievalChain.from_llm(llm=llm,chain_type='stuff',
+                                              retriever=vector_store.as_retriever(search_kwargs={"k":2}),
+                                              memory=memory)
+st.title("Medical Consultation AI👨‍⚕️")
 def conversation_chat(query):
     result = chain({"question": query, "chat_history": st.session_state['history']})
     st.session_state['history'].append((query, result["answer"]))
