6f2e00f756aeb5d3951c7a2d82f77aa702c75aa9
Sirajo Muazu Kallo <119134229+sirajoka@users.noreply.github.com>
2023-09-18 22:07:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,79 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import torch
+from torch import cuda, bfloat16
+import transformers
+from transformers import StoppingCriteria, StoppingCriteriaList
+from langchain.llms import HuggingFacePipeline
+
+model_id = 'meta-llama/Llama-2-7b-chat-hf'
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+# set quantization configuration to load large model with less GPU memory
+# this requires the `bitsandbytes` library
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+# begin initializing HF items, you need an access token
+hf_auth = HUGGIN
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+    use_auth_token=hf_auth
+)
+
+# enable evaluation mode to allow model inference
+model.eval()
+
+#print(f"Model loaded on {device}")
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+stop_list = ['\nHuman:', '\n```\n']
+
+stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
+stop_token_ids
+
+stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
+stop_token_ids
+
+
+# define custom stopping criteria object
+class StopOnTokens(StoppingCriteria):
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        for stop_ids in stop_token_ids:
+            if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
+                return True
+        return False
+
+stopping_criteria = StoppingCriteriaList([StopOnTokens()])
+
+generate_text = transformers.pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=True,  # langchain expects the full text
+    task='text-generation',
+    # we pass model parameters here too
+    stopping_criteria=stopping_criteria,  # without this model rambles during chat
+    temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+    max_new_tokens=512,  # max number of tokens to generate in the output
+    repetition_penalty=1.1  # without this output begins repeating
+)
+
 
 #load the pdf files from the path
 #loader = DirectoryLoader('data/',glob="*.pdf",loader_cls=PyPDFLoader)
@@ -25,14 +98,15 @@ text_chunks = text_splitter.split_documents(documents)
 
 #create embeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                   model_kwargs={'device':'cpu'})
+                                   model_kwargs={'device':'cuda'})
 
 #vectorstore
 vector_store = FAISS.from_documents(text_chunks,embeddings)
 
 #create llm
-llm = CTransformers(model="model.bin",model_type="llama",
-                    config={'max_new_tokens':128,'temperature':0.01}, streaming=True, callbacks=[StreamingStdOutCallbackHandler()])
+
+
+llm = HuggingFacePipeline(pipeline=generate_text)
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
@@ -53,7 +127,7 @@ def initialize_session_state():
         st.session_state['history'] = []
 
     if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
+        st.session_state['generated'] = ["Heyya, I'm Doc Nadya. how may I be of service?"]
 
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hey! 👋"]
