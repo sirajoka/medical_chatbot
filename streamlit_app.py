82a476de96e1008e5f60db8cd9661e9f6b61ef28
Sirajo Muazu Kallo <119134229+sirajoka@users.noreply.github.com>
2023-09-18 22:47:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import transformers
 from transformers import StoppingCriteria, StoppingCriteriaList
 from langchain.llms import HuggingFacePipeline
 import dotenv
+import os
 
 model_id = 'meta-llama/Llama-2-7b-chat-hf'
 device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
